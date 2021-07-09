5cd1722e7b5b1d5df082c2cce3c3158ec41d2396
aleale2121 <alefewyimer4@gmail.com>
2021-07-09 10:04:28 +0300

    initial commit and home page

---
+++ cmd/web/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/aleale2121/GoWebSocket_Chat/internal/handlers"
+	"github.com/bmizerany/pat"
+	"net/http"
+)
+
+func routes() http.Handler {
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+
+	return mux
+
+}
